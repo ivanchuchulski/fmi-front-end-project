acad9bc418630a83d4ed7e18287e0ac76c93e039
Ivan Chuchulski <zake6@abv.bg>
2021-01-27 18:18:09 +0200

    refactor statistics js

--- script/statistics.js
+++ script/statistics.js
@@ -1,37 +1,32 @@
-// using the javascript immediately-invoked function expression (IIFE)
+'use strict';
 (function () {
     window.onload = () => loadStatistics();
 
-    navigationButtonHandlers();
+    addNavbarHandlers();
 })();
 
-function navigationButtonHandlers() {
-    let schedulePageButton = document.getElementById("schedule-page-button");
-    schedulePageButton.addEventListener("click", () => {
-        window.location = "schedule.html";
-    });
+function addNavbarHandlers() {
+    let schedulePageButton =
+        document.getElementById("schedule-page-button")
+                .addEventListener("click", () => window.location = "schedule.html");
 
-    let personalSchedule = document
-        .getElementById("personalised-schedule-button")
-        .addEventListener("click", () => {
-            window.location = "personal-schedule.html";
-        });
-
+    let personalSchedule =
+        document.getElementById("personalised-schedule-button")
+                .addEventListener("click", () => window.location = "personal-schedule.html");
 
     let statisticsButton = document.getElementById("view-statistics");
-    statisticsButton.addEventListener("click", () => {
-        window.location = "statistics.html";
-    });
+    statisticsButton.addEventListener("click", () => window.location = "statistics.html");
+
     addHighlight(statisticsButton);
 
-    let logoutButton = document
-        .getElementById("logout-button")
-        .addEventListener("click", logoutRequest);
+    let logoutButton =
+        document.getElementById("logout-button")
+                .addEventListener("click", () => window.location = "index.html");
 }
 
 function loadStatistics() {
     const LOAD_SCHEDULE_METHOD = "GET";
-    const LOAD_SCHEDULE_URL = "php/api.php/statistics";
+    const LOAD_SCHEDULE_URL = "data/stats.json";
 
     ajaxLoadRequest(LOAD_SCHEDULE_URL, LOAD_SCHEDULE_METHOD);
 }
@@ -42,51 +37,48 @@ function ajaxLoadRequest(url, method, data) {
     xhr.addEventListener("load", () => ajaxLoadHandler(xhr));
 
     xhr.open(method, url, true);
-    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr.send(data);
 }
 
 function ajaxLoadHandler(xhr) {
+    const okResponseCode = 200;
+
+    let responseCode = xhr.status;
     let responseText = xhr.responseText;
 
-    if (responseText.success) {
+    if (responseCode === okResponseCode) {
         console.log("success load statistics");
         drawStatistics(responseText);
     } else {
         console.log("error : load statistics");
-        displayMessage(
-            "грешка : не е започната сесия или сесията е изтелкла(или файлът със събитията не може да бъде зареден)"
-        );
+        displayMessage("статистикака не може да бъде заредена");
     }
 }
 
-function drawStatistics(response) {
+function drawStatistics(responseText) {
     const TEXT_CENTER_CLASSNAME = "text-center";
+    let decodedResponseText = JSON.parse(responseText);
 
-    let statisticsList = response.data;
-    let topPresentations = response.topFivePresentations;
+    let {topFivePresentations, stats, numberOfUsers} = decodedResponseText;
 
-    let numberUsers = statisticsList.numberOfUsers;
-    let numberPresentations = statisticsList.numberOfPresentations;
-    let numberPreferences = statisticsList.numberOfPreferences;
-    let maxNumberOfPreference = statisticsList.maxNumberOfPreference;
-    let averageNumberOfPreference = statisticsList.averageNumberOfPreference;
-    let mostPreferredPresentation = statisticsList.mostPreferredPresentation;
+    let {numberOfPresentations, numberOfPreferences,
+        maxNumberOfPreferencesForUser, averageNumberOfPreference, mostPreferredPresentation} = stats;
 
     let userCountTableData = document.getElementById("user-count");
-    userCountTableData.innerText += numberUsers;
+    userCountTableData.innerText += numberOfUsers;
     userCountTableData.className += TEXT_CENTER_CLASSNAME;
 
     let presentationCountTableData = document.getElementById("presentation-count");
-    presentationCountTableData.innerText += numberPresentations;
+    presentationCountTableData.innerText += numberOfPresentations;
     presentationCountTableData.className += TEXT_CENTER_CLASSNAME;
 
     let preferenceCountTableData = document.getElementById("preference-count");
-    preferenceCountTableData.innerText += numberPreferences;
+    preferenceCountTableData.innerText += numberOfPreferences;
     preferenceCountTableData.className += TEXT_CENTER_CLASSNAME;
 
     let maxPreferenceCountTableData = document.getElementById("max-preference-count");
-    maxPreferenceCountTableData.innerText += maxNumberOfPreference;
+    maxPreferenceCountTableData.innerText += maxNumberOfPreferencesForUser;
     maxPreferenceCountTableData.className += TEXT_CENTER_CLASSNAME;
 
     let averagePreferenceCountTableData = document.getElementById("average-preference-count");
@@ -97,22 +89,26 @@ function drawStatistics(response) {
     mostPopularPresentationTableData.innerText += mostPreferredPresentation;
     mostPopularPresentationTableData.className += TEXT_CENTER_CLASSNAME;
 
-    let tbody = document.getElementById("statistics-top-five-body");
+    topFivePresentations.sort((left, right) => (left.count < right.count) ? 1 : -1);
 
     let positionCounter = 1;
-    topPresentations.forEach(presentation => {
-        let tr = document.createElement("tr");
+    let tableBody = document.getElementById("statistics-top-five-body");
 
-        tr.innerHTML += `<td>${positionCounter}. ${presentation.theme}</td>
-                        <td>${presentation.count} </td>`;
-                        
-        positionCounter++;
+    for (let sortedPresentation of topFivePresentations) {
+        let tableRow = document.createElement("tr");
 
-        tbody.appendChild(tr);
-    });
+        let tableDataFirst = document.createElement("td");
+        let tableDataSecond = document.createElement("td");
 
+        tableDataFirst.innerText += `${positionCounter++}. ${sortedPresentation.theme}`;
+        tableDataSecond.innerText += `${sortedPresentation.count}`;
+
+        tableRow.appendChild(tableDataFirst);
+        tableRow.appendChild(tableDataSecond);
+
+        tableBody.appendChild(tableRow);
+    }
 }
-
 
 function addHighlight(preferenceButton) {
     preferenceButton.className += " active";
@@ -122,40 +118,4 @@ function displayMessage(text) {
     let messageLabel = document.getElementById("messageLabel");
 
     messageLabel.innerText = text;
-}
-
-function goToSchedulePage(schedulePageUrl) {
-    window.location = schedulePageUrl;
-}
-
-function logoutRequest() {
-    const LOGOUT_URL = "php/api.php/logout";
-    const LOGOUT_METHOD = "POST";
-
-    ajaxLogoutRequest(LOGOUT_URL, LOGOUT_METHOD);
-}
-
-function ajaxLogoutRequest(url, method, data) {
-    let xhr = new XMLHttpRequest();
-
-    xhr.addEventListener("load", () => ajaxLogoutHandler(xhr));
-
-    xhr.open(method, url, true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send(data);
-}
-
-function ajaxLogoutHandler(xhr) {
-    let response = JSON.parse(xhr.responseText);
-
-    if (response.success) {
-        console.log("success : logout request");
-        goToLoginPage("index.html");
-    } else {
-        console.log("error : logout request");
-    }
-}
-
-function goToLoginPage(loginPageUrl) {
-    window.location = loginPageUrl;
 }
